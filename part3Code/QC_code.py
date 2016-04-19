d656b127b1ae8c30bfa2f61002bf463077b612bb
susanhao <susanhao@sas.upenn.edu>
2016-04-18 22:46:31 -0400

    Added files via upload

---
+++ part3Code/QC_code.py
@@ -0,0 +1,61 @@
+'''
+QC_code.py
+
+
+input headers from QC_input.csv:
+rap_theme
+description
+previous_lines
+possible_lines
+vote_numbers
+'''
+
+import csv
+
+if __name__ == '__main__' : 
+    # Create lists for desired objects
+    worker_ids = [];
+    gold_answers = [];
+    answers = [];
+    urls = [];
+
+    with open('QC_input.csv', 'rb') as full:
+        # Create reader object for the quality control input
+        reader = csv.reader(full)
+        
+        # Vote count default is 0
+        votes = 0
+        rap = ''
+
+        # Load into lists the desired information
+        for row in reader:
+            try:
+                current_vote = int(row[4])
+                if current_vote > votes:
+                    # Tracks winning rap and the vote count of winning rap
+                    votes = current_vote
+                    rap = row[3]
+            except:
+                continue
+    
+    with open('proto_in.tsv', 'r') as old:
+        old_reader = csv.reader(old, delimiter='\t')
+        next(old_reader)
+        for line in old_reader:
+            theme = line[0]
+            description = line[1]
+            previous_lines = line[2]
+        
+    # Update the lines
+    if (previous_lines == 'N/A'):
+        previous_lines = rap
+    else:
+        previous_lines = previous_lines + '; ' + rap
+        
+    # Update the new input file for the next iteration
+    with open('proto_in.tsv', 'wb') as f:
+        output = csv.writer(f, delimiter='\t')
+        headers = ['rap_theme', 'description', 'previous_lines']
+        output.writerow(headers)
+        row=[theme, description, previous_lines]
+        output.writerow(row)
---
+++ part3Code/votes_to_counts.py
@@ -0,0 +1,27 @@
+import sys
+import csv
+import json
+
+with open('produce_vote_output.csv', 'r') as infile:
+	reader = csv.DictReader(infile)
+	data = {}
+	for row in reader:
+		rap_theme = row['rap_theme']
+		description = row['description']
+		previous_lines = row['previous_lines']
+		line = row['possible_lines']
+		for header in row:
+			if header == 'should_this_line_be_added_to_the_above_rap':
+				if line not in data:
+					data[line] = 0
+				if row[header] == 'yes':
+					data[line] += 1
+
+
+output = csv.writer(open('QC_input.csv', 'w'))
+headers = ['rap_theme', 'description', 'previous_lines', 'possible_lines', 'vote_numbers']
+output.writerow(headers)
+
+for line in data:
+	row = [rap_theme, description, previous_lines, line, data[line]]
+	output.writerow(row)
