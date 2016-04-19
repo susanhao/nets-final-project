6073445be3c42bcc3a3ed019c83866b692bb570d
tpeterson993 <thpe@sas.upenn.edu>
2016-04-19 16:16:16 -0400

    Update bash.sh

--- part3Code/bash.sh
+++ part3Code/bash.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-
+export CROWDFLOWER_API_KEY=8kg2XeBus1yfQGx32Tgt
 (( i++ ))
-while [[ $i -lt 3 ]] ; do
+while [[ $i -lt 10 ]] ; do
     echo "ITERATION $i"
     python code_production.py create
     python code_production.py launch
     COUNTER=$((python code_production.py ping) 2>&1)
-        until [[ $COUNTER -lt 1 ]] ; do
-            echo Production judgments remainin: $COUNTER
+        until [[ $COUNTER -lt 1 ]]
+        do
+            echo Production judgments remaining are $COUNTER
             COUNTER=$((python code_production.py ping) 2>&1)
         done
     python code_production.py results
@@ -15,7 +16,8 @@ while [[ $i -lt 3 ]] ; do
     python code_voting.py create
     python code_voting.py launch
     COUNTER=$((python code_voting.py ping) 2>&1)
-        until [[ $COUNTER -lt 1 ]] ; do
+        until [[ $COUNTER -lt 1 ]]
+        do
             echo Voting judgments remaining are $COUNTER
             COUNTER=$((python code_voting.py ping) 2>&1)
         done
@@ -25,3 +27,4 @@ while [[ $i -lt 3 ]] ; do
     python QC_code.py
     (( i++ ))
 done
+
