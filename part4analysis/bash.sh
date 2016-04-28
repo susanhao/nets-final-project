49ef65e4ea14443035478435fad73863599570de
susanhao <susanhao@sas.upenn.edu>
2016-04-27 21:59:12 -0400

    Create bash.sh

---
+++ part4analysis/bash.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+export CROWDFLOWER_API_KEY=8kg2XeBus1yfQGx32Tgt
+(( i++ ))
+while [[ $i -lt 12 ]] ; do
+echo "ITERATION $i"
+python code_production.py create
+python code_production.py launch
+python code_production.py check
+python code_production.py results
+python code_voting.py create
+python code_voting.py launch
+python code_voting.py check
+python code_voting.py results
+python votes_to_counts.py
+python QC_code.py
+(( i++ ))
+done
